47c951e09b5cad3312606a291c34d936f32301ff
kylie-ng <kyliesk888@gmail.com>
2025-09-20 12:23:44 -0400

    navbar fix attempt

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from babel.dates import format_datetime
+
 app = Flask(__name__)
+Bootstrap(app)
 
-# Example 2-1
+# Activity 1.3: Home page
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    # Get current datetime
+    now = datetime.now()
+    # Babel uses "full" instead of "LLLL"
+    timestamp = format_datetime(datetime.now(), format='full')
+    return render_template('index.html', name="Kylie", timestamp=timestamp)
 
-# Example 2-2
+# User page (from Example 2-2, 3-3, but fixed)
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
-# Example 3-3
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name-name)
-
-# Example 3-6
+# Example 3-6 (Errror Handlers)
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
