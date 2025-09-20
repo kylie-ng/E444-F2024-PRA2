053b6cfa3f4476a4f1fff86a274db14f412df43e
kylie-ng <kyliesk888@gmail.com>
2025-09-20 14:50:48 -0400

    docker working on localhost5000!

--- app.py
+++ app.py
@@ -64,3 +64,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+# The debug=True and host="0.0.0.0" will: Show detailed errors in the logs. Make Flask accessible from outside the container
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
